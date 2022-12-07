ee6538feb1565db8fcf06112aec0bcdb1a4d7e77
nikhil shakya <nikhil.shakya@roado.co.in>
2022-12-07 15:32:41 +0530

    minor updates

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,18 +1,3 @@
-// const multer = require("multer");
-// const path = require("path");
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   },
-// });
-// const upload = multer({ storage: storage });
-
-// module.exports = upload;
-
 const path = require("path");
 const multer = require("multer");
 
